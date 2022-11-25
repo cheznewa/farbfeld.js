cac816eb129a8dfbdca50a1736dccaa4240122ad
Newa <chezsick@orange.fr>
2022-11-25 02:36:53 +0100

    svg now, no text

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ This Javascript Program, Working Render A Farbfeld Image In HTML
 You Insert The `farbfeld.js` And Insert In Script `farbfeld_render(varimg)` He Rendered Directly.
 ## How To Insert A Image Into Javascript?
 You Can Use [Base256](https://github.com/lvandeve/base256) For Insert A Image Into Javascript With The Arg `--format=js`.
-## The Compatibility?
-It Correctly Work In Chromium, Not With Firefox.
--- farbfeld.js
+++ farbfeld.js
@@ -8,6 +8,7 @@ if (102 == ff[0].charCodeAt() && 97 == ff[1].charCodeAt() && 114 == ff[2].charCo
 {
 var w = (ff[8].charCodeAt()*16777216)+(ff[9].charCodeAt()*65536)+(ff[10].charCodeAt()*256)+(ff[11].charCodeAt());
 var h = (ff[12].charCodeAt()*16777216)+(ff[13].charCodeAt()*65536)+(ff[14].charCodeAt()*256)+(ff[15].charCodeAt());
+document.write("<svg width=\""+w+"\" height=\""+h+"\">");
 for (var a=0;a<h;a++)
 {
 for (var b=0;b<w;b++)
@@ -16,13 +17,13 @@ red   = String(ff[16+0+(8*(b+(a*w)))].charCodeAt());
 green = String(ff[16+2+(8*(b+(a*w)))].charCodeAt());
 blue  = String(ff[16+4+(8*(b+(a*w)))].charCodeAt());
 alpha = String(ff[16+6+(8*(b+(a*w)))].charCodeAt());
-document.write("<span style=\"font-size:1px;color:rgba("+red+","+green+","+blue+","+alpha+");\">█</span>");
+document.write("<rect width=\"1\" height=\"1\" x=\""+b+"\" y=\""+a+"\" style=\"fill:rgba("+red+","+green+","+blue+","+alpha+");\"/>");
 }
-document.write("<br/>");
 }
+document.write("</svg>");
 }
 else
 {
-document.write("Not A Farbfeld")
+document.write("Not A Farbfeld");
 }
 }
