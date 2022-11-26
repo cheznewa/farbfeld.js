a215a4c3f313b9b759927b932e1dfc9668cebef0
Newa <chezsick@orange.fr>
2022-11-26 03:48:24 +0100

    add map effects

--- farbfeld.js
+++ farbfeld.js
@@ -42,15 +42,15 @@ for (var a=0;a<h;a++)
 {
 for (var b=0;b<w;b++)
 {
-red   = String(ff[16+0+(8*(b+(a*w)))].charCodeAt());
-green = String(ff[16+2+(8*(b+(a*w)))].charCodeAt());
-blue  = String(ff[16+4+(8*(b+(a*w)))].charCodeAt());
-alpha = String(ff[16+6+(8*(b+(a*w)))].charCodeAt());
+red   = ff[16+0+(8*(b+(a*w)))].charCodeAt();
+green = ff[16+2+(8*(b+(a*w)))].charCodeAt();
+blue  = ff[16+4+(8*(b+(a*w)))].charCodeAt();
+alpha = ff[16+6+(8*(b+(a*w)))].charCodeAt();
 if (mod == "add")
 {
-red   = Number(red)   + argv[0]
-green = Number(green) + argv[1]
-blue  = Number(blue)  + argv[2]
+red   = red   + argv[0]
+green = green + argv[1]
+blue  = blue  + argv[2]
 if (red > 255)
 red = 255
 if (green > 255)
@@ -60,9 +60,9 @@ blue = 255
 }
 if (mod == "sub")
 {
-red   = Number(red)   - argv[0]
-green = Number(green) - argv[1]
-blue  = Number(blue)  - argv[2]
+red   = red   - argv[0]
+green = green - argv[1]
+blue  = blue  - argv[2]
 if (red < 0)
 red = 0
 if (green < 0)
@@ -72,27 +72,80 @@ blue = 0
 }
 if (mod == "xor")
 {
-red   = Number(red)   ^ argv[0]
-green = Number(green) ^ argv[1]
-blue  = Number(blue)  ^ argv[2]
+red   = red   ^ argv[0]
+green = green ^ argv[1]
+blue  = blue  ^ argv[2]
 }
 if (mod == "bnw")
 {
-var base = Math.floor((Number(red)+Number(green)+Number(blue))/3)
+var base = Math.floor((red+green+blue)/3)
 red   = Math.floor(red*(1-(argv[0]/255)))+Math.floor(base*argv[0]/255)
 green = Math.floor(green*(1-(argv[1]/255)))+Math.floor(base*argv[1]/255)
 blue  = Math.floor(blue*(1-(argv[2]/255)))+Math.floor(base*argv[2]/255)
 }
 if (mod == "sep")
 {
-var baser = Math.floor((Number(red)+Number(green)+Number(blue))/3)
-var baseg = Math.floor((Number(red)+Number(green)+Number(blue))/4)
-var baseb = Math.floor((Number(red)+Number(green))/6)
-red   = Number(Math.floor(red*(1-(argv[0]/255))))+Number(Math.floor(baser*argv[0]/255))
-green = Number(Math.floor(green*(1-(argv[1]/255))))+Number(Math.floor(baseg*argv[1]/255))
-blue  = Number(Math.floor(blue*(1-(argv[2]/255))))+Number(Math.floor(baseb*argv[2]/255))
+var baser = Math.floor((red+green+blue)/3)
+var baseg = Math.floor((red+green+blue)/4)
+var baseb = Math.floor((red+green)/6)
+red   = Math.floor(red*(1-(argv[0]/255)))+Math.floor(baser*argv[0]/255)
+green = Math.floor(green*(1-(argv[1]/255)))+Math.floor(baseg*argv[1]/255)
+blue  = Math.floor(blue*(1-(argv[2]/255)))+Math.floor(baseb*argv[2]/255)
 }
-document.write("<rect width=\"1\" height=\"1\" x=\""+b+"\" y=\""+a+"\" style=\"fill:rgba("+red+","+green+","+blue+","+alpha+");\"/>");
+if (mod == "map")
+{
+z = []
+for (var m=0;m<argv.length;m++)
+{
+var ar = parseInt(argv[m][0] + argv[m][1],16)
+var ag = parseInt(argv[m][2] + argv[m][3],16)
+var ab = parseInt(argv[m][4] + argv[m][5],16)
+z = z.concat(Math.abs(red-ar)+Math.abs(green-ag)+Math.abs(blue-ab))
+}
+var d = 10000
+for (var n=0;n<argv.length;n++)
+{
+if (z[n] < d)
+{
+d = z[n]
+red = parseInt(argv[n][0] + argv[n][1],16)
+green = parseInt(argv[n][2] + argv[n][3],16)
+blue = parseInt(argv[n][4] + argv[n][5],16)
+}
+}
+}
+if (mod == "emp")
+{
+z = []
+for (var m=0;m<argv.length;m++)
+{
+var ar = parseInt(argv[m][0] + argv[m][1],16)
+var ag = parseInt(argv[m][2] + argv[m][3],16)
+var ab = parseInt(argv[m][4] + argv[m][5],16)
+z = z.concat(Math.abs(red-ar)+Math.abs(green-ag)+Math.abs(blue-ab))
+}
+var d = 10000
+var ll = Math.floor(255.0/argv.length)
+for (var n=0;n<argv.length;n++)
+{
+if (z[n] < d)
+{
+d = z[n]
+r = n*ll
+}
+}
+red = r
+green = r
+blue = r
+}
+if (mod == "dmp")
+{
+var m = Math.floor(red/Math.floor(255/argv.length))
+var red = parseInt(argv[m][0] + argv[m][1],16)
+var green = parseInt(argv[m][2] + argv[m][3],16)
+var blue = parseInt(argv[m][4] + argv[m][5],16)
+}
+document.write("<rect width=\"1\" height=\"1\" x=\""+b+"\" y=\""+a+"\" style=\"fill:rgba("+String(red)+","+String(green)+","+String(blue)+","+String(alpha)+");\"/>");
 }
 }
 document.write("</svg>");
